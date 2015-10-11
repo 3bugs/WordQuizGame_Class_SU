8944992d7187c3f345d56f2fa25bf7def207bf47
Promlert Lovichit <promlert@gmail.com>
2015-10-11 13:51:20 +0700

    Add getImageFileNames method in GameActivity

--- app/src/main/java/com/example/wordquizgame/GameActivity.java
+++ app/src/main/java/com/example/wordquizgame/GameActivity.java
@@ -1,6 +1,7 @@
 package com.example.wordquizgame;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
@@ -61,7 +63,15 @@ public class GameActivity extends AppCompatActivity {
 
         Log.i(TAG, "Number of choices: " + mNumChoices);
 
+        mFileNameList = new ArrayList<>();
+        mQuizWordList = new ArrayList<>();
+        mChoiceWordList = new ArrayList<>();
+
+        mRandom = new Random();
+        mHandler = new Handler();
+
         setupViews();
+        getImageFileNames();
     }
 
     private void setupViews() {
@@ -69,5 +79,30 @@ public class GameActivity extends AppCompatActivity {
         mQuestionImageView = (ImageView) findViewById(R.id.questionImageView);
         mButtonTableLayout = (TableLayout) findViewById(R.id.buttonTableLayout);
         mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+    }
+
+    private void getImageFileNames() {
+        String[] categories = {"animals", "body", "colors", "numbers", "objects"};
+
+        AssetManager assets = getAssets();
+
+        for (String category : categories) {
+            try {
+                String[] fileNames = assets.list(category);
+
+                for (String f : fileNames) {
+                    mFileNameList.add(f.replace(".png", ""));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "Error listing files in " + category);
+            }
+        }
+
+        Log.i(TAG, "***** รายชื่อไฟล์ภาพทั้งหมด *****");
+        for (String f : mFileNameList) {
+            Log.i(TAG, f);
+        }
     }
 }
